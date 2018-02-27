705215f504a90718557a0995b2d75b0ecb505f23
DESKTOP-2SNSRJH\Mengty <vongmengty55@gmail.com>
2018-02-27 22:33:31 +0700

    fix timestamps in model

--- application/models/Product_category_model.php
+++ application/models/Product_category_model.php
@@ -3,7 +3,8 @@ class Product_category_model extends MY_Model {
     public $table = 'productcategories'; // you MUST mention the table name
   	public $primary_key = 'CategoryID'; // you MUST mention the primary key
   	public $fillable = array(); // If you want, you can set an array with the fields that can be filled by insert/update
-  	//public $protected = array(); // ...Or you can set an array with the fields that cannot be filled by insert/update
+    public $timestamps = FALSE;
+    //public $protected = array(); // ...Or you can set an array with the fields that cannot be filled by insert/update
   	public function __construct()
   	{
   		parent::__construct();
