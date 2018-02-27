0505c80b591ca817f2280080f2233e4f742e407e
DESKTOP-2SNSRJH\Mengty <vongmengty55@gmail.com>
2018-02-27 22:19:31 +0700

    fix user_model

--- application/models/User_model.php
+++ application/models/User_model.php
@@ -2,7 +2,7 @@
 class User_model extends MY_Model {
     public $table = 'users'; // you MUST mention the table name
   	public $primary_key = 'UserID'; // you MUST mention the primary key
-  	public $fillable = array(); // If you want, you can set an array with the fields that can be filled by insert/update
+  	public $fillable = array('UserEmail','UserPassword','UserFirstName','UserLastName','UserCity','UserState','UserZip','UserEmailVerified','UserRegistrationDate','UserVerificationCode','UserIP','UserPhone','UserFax','UserCountry','UserAddress','UserAddress2'); // If you want, you can set an array with the fields that can be filled by insert/update
   	//public $protected = array(); // ...Or you can set an array with the fields that cannot be filled by insert/update
   	public function __construct()
   	{
